decbc4bac74601e36059607c72effdeca8b523fe
Lincunia <pll52742@gmail.com>
2022-09-13 18:52:31 -0500

    sample to her

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./css/style.css"></link>
+	<title>Nombre del proyecto</title>
+    </head>
+    <body style="background-color: #edfafc;">
+	<header style="background-color: #ffa6c5;">
+	    <h1>Prueba de cabecera</h1>
+	</header>
+	<div class="conjunction">
+	    <section style="background-color: #93f596;">
+		<article style="background-color: #e7f797;">
+		    <p>Sollicitudin venenatis, nibh libero
+		    hendrerit sem, eu tempor nisi felis et metus. Etiam gravida sem ut mi. Integer
+		    volutpat, enim eu varius gravida, risus urna venenatis lectus, ac ultrices quam
+		    nulla eu leo. Duis arcu. Class aptent taciti sociosqu ad litora torquent per
+		    conubia nostra, per inceptos himenaeos.</p>
+
+		    <p>Vivamus lacus libero, aliquam eget, iaculis quis, tristique adipiscing,
+		    diam. Vivamus nec massa non justo iaculis pellentesque. Aenean accumsan elit
+		    sit amet nibh feugiat semper. Cras tempor ornare purus. Integer id nisi.
+		    Phasellus dui velit, ultrices vel, ullamcorper mattis, hendrerit in, erat.
+		    Aenean vel quam at eros mattis commodo. Aenean feugiat iaculis justo.
+		    Maecenas.</p> <!--/lorem-->
+		</article>
+		<article style="background-color: #f7e4a6;"> <!--lorem--> <p>Egestas.
+		    Praesent lacus diam, auctor quis, venenatis in, hendrerit at, est. Vivamus eget
+		    eros. Phasellus congue, sapien ac iaculis feugiat, lacus lacus accumsan lorem,
+		    quis volutpat justo turpis ac mauris.</p>
+
+		    <p>Duis velit magna, scelerisque vitae, varius ut, aliquam vel, justo. Proin ac
+		    augue. Nullam auctor lectus vitae arcu. Vestibulum porta justo placerat purus.
+		    Ut sem nunc, vestibulum nec, sodales vitae, vehicula eget, ipsum. Sed nec
+		    tortor. Aenean malesuada. Nunc convallis, massa eu vestibulum commodo, quam
+		    mauris interdum arcu, at pellentesque diam metus ut nulla. Vestibulum eu dolor
+		    sit amet lacus varius fermentum. Morbi dolor enim, pulvinar eget, lobortis ac,
+		    fringilla ac, turpis. </p> <!--/lorem-->
+		</article>
+	    </section>
+	    <aside style="background-color: #b597f7;">
+		<p>Nisl ac orci.
+		Donec non nisl. Mauris lacus sapien, congue a, facilisis at, egestas vel, quam.
+		Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+		cubilia Curae.</p>
+
+		<p>Phasellus ipsum odio, suscipit nec, fringilla at, vehicula quis, tellus.
+		Phasellus gravida condimentum dui. Aenean imperdiet arcu vitae ipsum. Duis
+		dapibus, nisi non porttitor iaculis, ligula odio sollicitudin mauris, non
+		luctus nunc massa a velit. Fusce ac nisi. Integer volutpat elementum metus.
+		Vivamus luctus ultricies diam. Curabitur euismod. Vivamus quam. Nunc ante.
+		Nulla mi nulla, vehicula nec, ultrices a, tincidunt vel, enim.</p>
+
+		<p>Suspendisse potenti. Aenean sed velit. Nunc a urna quis turpis imperdiet
+		sollicitudin. </p> <!--/lorem-->
+	    </aside>
+	</div>
+    </body>
+</html>
